798a200c078d80b15b8e8eb3db663314f78dd096
rehan <12150113827@students.uin-suska.ac.id>
2023-04-12 00:10:55 +0700

    tugas

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,25 @@
+<template>
+    <WelcomeItem>
+        <div id="app">
+    <router-view></router-view>
+  </div>
+  <h2 align="center">Login akun</h2>
+
+  <center>
+    <label for="usname">Username:</label>
+    <input type="text" id="usname" name="usname"><br><br>
+    <label for="password">Pasworrd:</label>
+    <input type="text" id="Password" name="Password"><br><br>
+    <input type="submit" value="Masuk">
+  </center>
+    </WelcomeItem>
+</template>
+
+<script>
+import WelcomeItem from './WelcomeItem.vue';
+
+export default {
+    name: "Login.vue",
+    components: { WelcomeItem }
+}
+</script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,58 @@
+<template>
+  <About>
+      <h1 style="background-color:powderblue;"><center>PRIBADI</center></h1>
+<center><img src="https://www.glngirwn.com/wp-content/uploads/2019/11/logo-design-trends-animated-cartoony-logos-example-1.gif" alt="gambar tidak muncul" style = " widht:400px; height:200px"> </center>
+
+
+<center><table border ="1">
+          <tr>
+          <th> <h2> TEST PEMBUATAN </h2> </th> 
+          </tr>
+          <tr><td> <h3> <center>TUGAS PW</center> </h3> </td></tr>
+      </table></center>
+
+
+<div style="background-color:whitesmoke;">
+  <h4 style="color:black"> PROFIL </h4> 
+<h5 style="color: black;">
+  Nama  : REHAN NUGRAHA TANJUNG <br>
+  NIM   : 12150113827 <br>
+</h5>
+<h6 style="color: crimson;">
+  <a href="https://www.instagram.com/rehannugraha06/" target="_blank">Social Media</a> </h6>
+</div>
+
+<form>
+  <p> Masukan Data </p>
+  <input placeholder="Name"/><br>
+  <input placeholder="Email"/><br>
+  <input placeholder="Alamat" required /><br>
+  <input placeholder="Nomor HP" required /><br>
+  <textarea placeholder="Message" required ></textarea><br>
+  <input type="submit" value="SEND"/>
+</form>
+
+
+<form action="/action_page.php">
+      <p><label for="Cofee Shop">Kritik dan Saran</label></p>
+      <textarea id="w3review" name="w3review" rows="4" cols="50">....</textarea>
+      <br>
+  </form>
+<br><br>
+<input type="submit" value="Submit">
+
+<form action="/action_page.php">
+
+<p> <span>Terima Kasih</span>  telah berkunjung LocalHost yang simpel ini kritik dan saran akan membantu menjadi langkah tujuan pembelajaran </p>
+
+<label for="nilai">Beri Penilaian:</label>
+<select name="nilai" id="nilai">
+  <option value="5">5 BINTANG</option>
+  <option value="4">4 BINTANG</option>
+  <option value="3">3 BINTANG</option>
+  <option value="2">2 BINTANG</option>
+  <option value="1">1 BINTANG</option>
+</select>
+</form>
+</About>
+</template>
